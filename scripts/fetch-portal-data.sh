799793dba373c7edd2646b1c4607fe992accf8ec
Billy Charlton <billy@okbecause.com>
2018-09-12 15:49:41 +0200

    fix #68: don't erase map if import fails

--- scripts/fetch-portal-data.sh
+++ scripts/fetch-portal-data.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
-set -ex
+set -e
+
+export LOGFILE=/tmp/psql-`date +%Y-%m-%d--%H:%M`.log
 
 # note this must be run as user postgres; i.e.
-#   sudo -u postgres fetch-portal-data.sh
+#   sudo -u postgres fetchportal-data.sh
 
-curl -X GET -H "X-USER-TOKEN: e050e01c-3176-cad7-b714-f8f75a6adb6d" \
+curl -Ss -X GET -H "X-USER-TOKEN: e050e01c-3176-cad7-b714-f8f75a6adb6d" \
      -H "Content-Type: application/csv" \
      "https://portal.sfcta.org/api/v1/project_locations" > /tmp/portal-dump.csv
 
-psql -f import-portal-data.sql geo
+# Does file exist and have size greater than 0
+if [ ! -s /tmp/portal-dump.csv ]; then
+        exit 0
+fi
+
+psql -q -f import-portal-data.sql geo &> $LOGFILE
+
+rm /tmp/portal-dump.csv

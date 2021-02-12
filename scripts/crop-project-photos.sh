0b513d2c391da45004d29628b00f5f13dbc801e0
Billy Charlton <billy@okbecause.com>
2021-02-12 14:46:47 +0100

    updated crop script

--- scripts/crop-project-photos.sh
+++ scripts/crop-project-photos.sh
@@ -7,18 +7,29 @@
 # Requires GraphicsMagick 'gm' utility
 # Requires Node.js
 #
-set -ex
+# Usage:  cd .../scripts
+#         bash ./crop-project-photos.sh /path/to/image/folder
 
-SRC_FOLDER=~/gdrive-okbecause/mystreet-project-images
+set -euo pipefail
+IFS=$'\n\t'
+shopt -s extglob
 
-THUMB_FOLDER=../static/project-thumbnails
+ROOT_FOLDER=`pwd`
+SRC_FOLDER="$1"
+
+THUMB_FOLDER="$ROOT_FOLDER/../static/project-thumbnails"
+
+echo $THUMB_FOLDER
 
 # TO-DO should pull images from google-drive first
 cp $SRC_FOLDER/* $THUMB_FOLDER
 
-for image in $THUMB_FOLDER/{*.JPG,*.jpg,*.png};
+cd "$THUMB_FOLDER"
+
+for image in @(*JPG|*jpg|*png|*PNG);
 do
     gm convert \
+          -verbose \
           -geometry 500x500^ \
           -gravity center \
           -crop 500x500 \
@@ -28,6 +39,11 @@ do
 done
 
 # create the JSON lookup file
-THUMB_FOLDER=$THUMB_FOLDER node process-project-images.js
+cd $ROOT_FOLDER
+THUMB_FOLDER="$THUMB_FOLDER" node process-project-images.js
+cd $THUMB_FOLDER
+rm @(*JPG|*jpg|*png|*PNG)
+echo "----ALL DONE ---"
+echo "Add, commit, and push all changes to the repository now"
 
-rm $THUMB_FOLDER/{*.JPG,*.jpg,*.png}
+
